98143978804245ebd68d847888a0f286866b2737
wagner lira <wagnermflira@gmail.com>
2022-04-01 18:21:40 -0300

    Atualização dados frontend

--- frontend/src/pages/EditCadastros/index.jsx
+++ frontend/src/pages/EditCadastros/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-//import { get } from 'lodash'
+import { get } from 'lodash'
 //import { isEmail, isInt, isFloat } from 'validator'
 import PropTypes from 'prop-types'
 import { toast } from 'react-toastify'
@@ -11,99 +11,89 @@ import { Container } from '../../styles/GlobalStyles'
 import { Form, ProfilePicture, Title } from './styled'
 
 
-export default function EditCadastros({ match, history }) {
+export default function EditCadastro({ match, history }) {
+  const id = get(match, 'params.id', '')
+
+  const [nome, setNome] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [inome, setINome] = useState([])
+  const [icpf, setICpf] = useState([])
+  
 
 
   useEffect(() => {
     async function getData() {
+      try {
+        const { data } = await axios.get(`/cadastro/${id}`)
+        setNome(data.nome)       
+        setCpf(data.cpf)
+      } catch (err) {
+        const status = get(err, 'response.status', 0)
+        const errors = get(err, 'response.data.errors', [])
+
+        if (status === 400) errors.map(error => toast.error(error))
+        history.push('/')
+      }
+    }
+    getData()
+    })
+    
+      {
+        /*Envio de dados  */
+      }
+    const handleSubmit = async e => {
+      e.preventDefault()
+      let formErrors = false
       
-  }})
-  {
-    /* Início extrair dados de setores */
-  }
-  const getSetores = async () => {
-   
-  }
-  {
-    /* Fim extrair dados de setores */
-  }
-  {
-    /* Início carregar dados de setores */
-  }
-  const loadSetores = () => {
-   
-  }
-  {
-    /* Fim carregar dados de setores */
-  }
-  {
-    /* Início carregar options select-setores */
-  }
-  const loadSetoresId = () => {
-    
-  }
-  {
-    /* Fim carregar options select-setores */
-  }
-
-  {
-    /* Inicio Envio de dados  */
-  }
-  const handleSubmit = async e => {
-    
-  }
-
+      if (formErrors) return
+  
+      try {
+        await axios.put(`/cadastro/${id}`, {
+          nome,
+          cpf
+        })
+        toast.success('Cadastro editado com sucesso!')
+        history.push('/')
+      } catch (err) {
+        const status = get(err, 'response.status', 0)
+        const data = get(err, 'response.data', {})
+        const errors = get(data, 'errors', [])
+  
+        if (errors.length > 0) {
+          errors.map(error => toast.error(error))
+        } else {
+          toast.error('Erro desconhecido')
+        }
+  
+      }
+    }
+  
+      {/* Fim Envio de dados  */}
   return (
     <Container>
-
       <Title>
-        Impressora: {} <br /> Patrimonio: {}
+        Editar dados cadastrais
       </Title>
-
-      <Form >
-        <div>
-          <label>Tipo:</label>
-          <select
-            class="form-select form-select- mb-3"
-          >
-            <option></option>
-            <option>Preto e Branco</option>
-            <option>Colorido</option>
-          </select>
-        </div>
-        <label>Patrimônio:</label>
-        <input
-          type="number"
-        />
-        <label>Número de Série:</label>
+      <Form onSubmit={handleSubmit}>
+      <label>Nome:</label>
         <input
           type="text"
+          value={nome}
+          onChange={e => setNome(e.target.value)}
         />
-        <div>
-          <label>Setor:</label>
-          <select
-            class="form-select form-select- mb-3"
-          >
-          </select>
-        </div>
-        <div>
-          <label>Status:</label>
-          <select
-            class="form-select form-select- mb-3"
-          >
-            <option></option>
-            <option>Ativo</option>
-            <option>Inativo</option>
-          </select>
-        </div>
-
+        <label>CPF:</label>
+        <input
+          type="number"
+          value={cpf}
+          onChange={e => setCpf(e.target.value)}
+        />
         <button type="submit">Enviar</button>
       </Form>
     </Container>
   )
 }
 
-EditCadastros.propTypes = {
+EditCadastro.propTypes = {
   match: PropTypes.shape({}).isRequired,
   history: PropTypes.shape([]).isRequired
 }
